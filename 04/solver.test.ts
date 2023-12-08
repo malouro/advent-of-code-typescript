aaba7075582db3ed128e6ff51839bf904837a345
Mike A. Louro <mike.a.louro@gmail.com>
2023-12-08 17:12:58 -0500

    feat(Day04): part one solution + tests

--- 04/solver.test.ts
+++ 04/solver.test.ts
@@ -7,5 +7,10 @@ describe('Day 3', () => {
 
       expect(answer).toEqual(13);
     });
+    test('solves correctly with sample input', async () => {
+      const answer = await solver('04/star.txt');
+
+      expect(answer).toEqual(20407);
+    });
   });
 });
--- 04/solver.ts
+++ 04/solver.ts
@@ -3,9 +3,32 @@ import { PathLike } from 'node:fs';
 
 export default async function (inputFile: string | PathLike): Promise<number> {
   const input = await readFile(inputFile, { encoding: 'utf-8' });
-  const lines = input.split('\n');
+  const cards = input.split('\n');
 
-  console.log(lines);
+  const cardResults: Map<number, number> = new Map();
 
-  return 0;
+  for (const card of cards) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [[cardId], winningNumbers, cardNumbers] = card
+      .split(/:|\|/)
+      .map((entry) => entry.replace(/Card\s+/g, '').trim())
+      .map((entry, i) => (i > 0 ? entry.split(/\s+/) : [entry]))
+      .map((section: string[]): number[] =>
+        section.map((entry: string): number => Number.parseInt(entry)),
+      );
+
+    let winningCount = 0;
+
+    console.log(cardId, winningNumbers, cardNumbers);
+
+    cardNumbers.forEach((number: number) => {
+      if (winningNumbers.some((n: number) => n === number)) {
+        winningCount++;
+      }
+    });
+
+    cardResults.set(cardId, Math.floor(Math.pow(2, winningCount - 1)));
+  }
+
+  return Array.from(cardResults.values()).reduce((acc: number, result: number) => acc + result, 0);
 }
