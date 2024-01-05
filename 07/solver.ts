a99c070085f6c2151bf9fe3d0760c2fe4c4b66f0
Mike A. Louro <mike.a.louro@gmail.com>
2024-01-05 18:47:19 -0500

    [Day 07] Implement Poker solution
    This solution is wrong because the problem doesn't really want exact Poker rules.
    (Boo.)

--- 07/solver.ts
+++ 07/solver.ts
@@ -1,27 +1,14 @@
 import { readInputFile } from '@helpers/fs';
 import { PathLike } from 'node:fs';
 
-type HandValue = {
-  /**
-   * - 0: High card
-   * - 1: Pair
-   * - 2: Two-pair
-   * - 3: Three-of-a-kind
-   * - 4: Fullhouse
-   * - 5: Four-of-a-kind
-   * - 6: Five-of-a-kind
-  */
-  handType: number;
-  value: number[];
-}
-
 /**
  * @param {number[]} hand Array of numbers that represent the card values.
  * @returns {HandValue}
  */
-function getHandValue(hand: number[]): HandValue {
+function getHandValue(hand: number[]): { handType: number; values: number[] } {
   const matches = new Map();
-  const values = []
+  let handType = 0;
+  let values: number[] = [];
 
   for (const card of hand) {
     if (!matches.has(card)) {
@@ -31,71 +18,134 @@ function getHandValue(hand: number[]): HandValue {
     }
   }
 
-  let pairs = [];
+  const cardValues = Array.from(matches.values());
+  const pairTypes = cardValues.filter((val) => val !== 1);
+  const pairValues = Array.from(matches.keys()).filter((val) => matches.get(val) !== 1);
 
-  Array.from(matches.keys()).forEach((card: number) => {
-    const numberOfThisCard = matches.get(card);
-
-    if (numberOfThisCard > 1) {
-      pairs.push(card);
+  if (pairTypes.includes(5)) {
+    handType = 6;
+    values.push(pairValues[0]);
+  } else if (pairTypes.includes(4)) {
+    handType = 5;
+    values.push(pairValues[0]);
+  } else if (pairTypes.includes(3)) {
+    if (pairValues.includes(2)) {
+      handType = 4;
+      values.push(Math.max(...pairValues));
+      values.push(Math.min(...pairValues));
+    } else {
+      handType = 3;
+      values.push(pairValues[0]);
     }
-  })
+  } else if (pairTypes.includes(2)) {
+    if (pairTypes.length > 1) {
+      handType = 2;
+      values.push(Math.max(...pairValues));
+      values.push(Math.min(...pairValues));
+    } else {
+      handType = 1;
+      values.push(pairValues[0]);
+    }
+  }
+
+  values.push(...hand.sort((a, b) => b - a).filter((v) => !values.includes(v)));
 
   return {
-    handType: 0,
-    value: []
-  }
+    handType,
+    values,
+  };
 }
 
-function convertToHex(cardValue: string): number {
-  if (cardValue.length > 1) {
-    throw new Error([
-      '`cardValue` can only be a single character.',
-      'Given string had a length larger than 1; did something go wrong?'
-    ].join('\n'))
+function convertToNumber(cardValue: string): number {
+  if (/[2-9]/.test(cardValue)) {
+    return parseInt(cardValue);
   }
-
-  switch (true) {
-    case /[2-9]/.test(cardValue):
-      return parseInt(cardValue, 16)
-    case /T/.test(cardValue):
-      return 0xA
-    case /J/.test(cardValue):
-      return 0xB
-    case /Q/.test(cardValue):
-      return 0xC
-    case /K/.test(cardValue):
-      return 0xD
-    case /A/.test(cardValue):
-      return 0xE
+  switch (cardValue) {
+    case 'T':
+      return 10;
+    case 'J':
+      return 11;
+    case 'Q':
+      return 12;
+    case 'K':
+      return 13;
+    case 'A':
+      return 14;
     default:
-      console.info(`No matching card value for ${cardValue} found; returning 0.`)
+      console.info(`No matching card value for ${cardValue} found; returning 0.`);
       return 0;
   }
 }
 
+type Hand = {
+  /**
+   * - 0: High card
+   * - 1: Pair
+   * - 2: Two-pair
+   * - 3: Three-of-a-kind
+   * - 4: Fullhouse
+   * - 5: Four-of-a-kind
+   * - 6: Five-of-a-kind
+   */
+  handType: number;
+  values: number[];
+  bet: number;
+};
+
 type Day7Solution = {
   partOne: number;
-}
+};
 
 export default async function solver(inputFile: string | PathLike): Promise<Day7Solution> {
   const input = (await readInputFile(inputFile)).split('\n');
 
-  const hands: number[][] = input
-    .map((line) => [...line.split(/\s/g)[0]]
-    .map((card) => convertToHex(card)))
-  const bets: number[] = input.map((line) => parseInt(line.split(/\s/g)[1], 10))
+  const bets: number[] = input.map((line) => parseInt(line.split(/\s/g)[1], 10));
+  const hands: Hand[] = input
+    .map((line) => [...line.split(/\s/g)[0]].map((card) => convertToNumber(card)))
+    .map((hand, i) => {
+      const { handType, values } = getHandValue(hand);
 
-  console.log(hands, bets)
+      return {
+        handType,
+        values,
+        bet: bets[i],
+      };
+    });
 
-  for (const hand of hands) {
-    const { handType, value } = getHandValue(hand);
+  for (let i = 0; i < hands.length; ++i) {
+    for (let j = 0; j < hands.length - 1 - i; ++j) {
+      if (hands[j].handType < hands[j + 1].handType) {
+        const tmp = hands[j];
+        hands[j] = hands[j + 1];
+        hands[j + 1] = tmp;
+      }
+      if (hands[j].handType === hands[j + 1].handType) {
+        for (let k = 0; k < hands[j].values.length; ++k) {
+          const firstVal = hands[j].values[k];
+          const secondVal = hands[j + 1].values[k];
 
-    console.log('Hand type: ', handType)
-    console.log('Hand value: ', value)
+          if (firstVal < secondVal) {
+            const tmp = hands[j];
+            hands[j] = hands[j + 1];
+            hands[j + 1] = tmp;
+            break;
+          }
+          if (firstVal > secondVal) {
+            break;
+          }
+        }
+      }
+    }
   }
+
+  console.log('Sorted hands: ', hands);
+
+  const partOneResult = hands.reduce(
+    (prev, next, index, arr) => prev + next.bet * (arr.length - index),
+    0,
+  );
 
   return {
-    partOne: 0
-  }
+    partOne: partOneResult,
+  };
 }
