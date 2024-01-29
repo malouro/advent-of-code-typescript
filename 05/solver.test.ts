951b24845d05a9a4ee252ff98cdcdea68369c381
Mike A. Louro <mike.a.louro@gmail.com>
2024-01-28 23:53:58 -0500

    [Day 5] Part two solution

--- 05/solver.test.ts
+++ 05/solver.test.ts
@@ -1,10 +1,23 @@
 import solver from './solver';
 
 describe('Day 5', () => {
-  test('solves correctly with sample input', async () => {
-    expect((await solver('05/sample1.txt')).partOne).toBe(35);
+  describe('Part One', () => {
+    test('solves correctly with sample input', async () => {
+      expect((await solver('05/sample1.txt')).partOne).toBe(35);
+    });
+    test('solves correctly with puzzle input', async () => {
+      expect((await solver('05/star.txt')).partOne).toBe(84470622);
+    });
   });
-  test('solves correctly with puzzle input', async () => {
-    expect((await solver('05/star.txt')).partOne).toBe(84470622);
+
+  describe('Part Two', () => {
+    test('solves correctly with sample input', async () => {
+      expect((await solver('05/sample1.txt')).partTwo).toBe(46);
+    });
+    // Unskip if you're running the part two solution.
+    // The brute force approach sucked so bad that it takes literal hours to run.
+    test.skip('solves correctly with puzzle input', async () => {
+      expect((await solver('05/star.txt')).partTwo).toBe(26714516);
+    });
   });
 });
--- 05/solver.ts
+++ 05/solver.ts
@@ -2,14 +2,38 @@ import { readInputFile } from '@helpers/fs';
 
 type Day5ReturnValue = {
   partOne: number;
+  partTwo: number;
 };
 
 export default async function (inputFile: string): Promise<Day5ReturnValue> {
   const input = (await readInputFile(inputFile)).split(/[\n\r][\n\r]/);
-  const seeds = input[0]
+  const seeds = input[0] // for part one
     .split(': ')[1]
     .split(' ')
     .map((x) => parseInt(x));
+  // `eslint-disable` here while part two soultion is skipped.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const seedySeeds = input[0] // for part two
+    .split(': ')[1]
+    .split(' ')
+    .map((x, index, arr): [number, number] | undefined => {
+      if (index % 2 === 0) {
+        return [parseInt(x), parseInt(arr[index + 1])];
+      }
+      return undefined;
+    })
+    .map((x) => {
+      if (typeof x !== 'undefined') {
+        const arr: number[] = [];
+        const [value, range]: [value: number, range: number] = x;
+
+        arr.push(value);
+        arr.push(value + range - 1);
+        return arr;
+      }
+    })
+    .filter((x) => typeof x !== 'undefined') as unknown as [number, number][];
+
   const maps = [...input.slice(1)].map((x) =>
     x
       .split('\n')
@@ -17,6 +41,9 @@ export default async function (inputFile: string): Promise<Day5ReturnValue> {
       .map((y) => y.split(' ').map((z) => parseInt(z))),
   );
   const partOneResult = [];
+  // `eslint-disable` here while part two soultion is skipped.
+  // eslint-disable-next-line prefer-const
+  let partTwoResult = -1;
 
   function translate(index: number, value: number) {
     if (index == maps.length) return value;
@@ -33,7 +60,36 @@ export default async function (inputFile: string): Promise<Day5ReturnValue> {
     partOneResult.push(translate(0, seed));
   }
 
+  /*
+  // This brute force approach is ridiculously unperformant.
+  // Uncomment for the part two result, or whatever.
+  // :^)
+  for (const seed of seedySeeds) {
+    const [start, end] = seed;
+
+    for (let i = 0; i < end - start; ++i) {
+      const res = translate(0, start + i);
+
+      if (partTwoResult === -1) {
+        partTwoResult = res;
+      } else if (res < partTwoResult) {
+        partTwoResult = res;
+      }
+
+      // console.log(
+      //   'Result is',
+      //   res,
+      //   'for value:',
+      //   start + i + '.',
+      //   'Min currently is',
+      //   partTwoResult,
+      // );
+    }
+  }
+  */
+
   return {
     partOne: Math.min(...partOneResult),
+    partTwo: partTwoResult,
   };
 }
