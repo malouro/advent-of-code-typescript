5a379c02a7141091757321ec6e5e89b63e928ca6
Mike A. Louro <mike.a.louro@gmail.com>
2025-12-05 14:02:52 -0500

    [2025][Day 2] Solution + test + notes

---
+++ src/2025/02/solver.test.ts
@@ -0,0 +1,40 @@
+import solver from './solver';
+
+describe('Day 2', () => {
+  describe('Part One', () => {
+    describe('Unit test', () => {
+      test.skip('sandbox testing', async () => {
+        const output = await solver('src/2025/02/fixtures/sample.txt');
+        console.log(output)
+      })
+    })
+    describe('Submission', () => {
+      test('solves correctly with sample input', async () => {
+        expect((await solver('src/2025/02/fixtures/sample.txt')).partOne).toBe(1227775554);
+      });
+
+      test('solves correctly with puzzle input', async () => {
+        expect((await solver('src/2025/02/star.txt')).partOne).toBe(34826702005);
+      });
+    })
+  });
+
+  describe('Part Two', () => {
+    describe('Unit test', () => {
+      test.skip('sandbox testing', async () => {
+        const { partTwo } = await solver('src/2025/02/fixtures/sample.txt');
+        console.log(partTwo)
+      })
+    });
+
+    describe('Submission', () => {
+      test('solves correctly with sample input', async () => {
+        expect((await solver('src/2025/02/fixtures/sample.txt')).partTwo).toBe(4174379265);
+      });
+
+      test('solves correctly with puzzle input', async () => {
+        expect((await solver('src/2025/02/star.txt')).partTwo).toBe(43287141963);
+      });
+    })
+  });
+});
---
+++ src/2025/02/solver.ts
@@ -0,0 +1,89 @@
+import { readInputFile, FsPathLike } from '@helpers/fs';
+import { number } from 'yargs';
+
+type Day2Solution = {
+  partOne: any;
+  partTwo: any;
+};
+
+// AUTHOR NOTE:
+// Regex is slow for this, in retrospect.
+// Didn't realize when I first wrote the implementation that an invalid ID
+// requires a repetition of digits EXACTLY twice. (reading comprehension baby)
+// Splitting the string down the middle is WAY better for this.
+// ...But who knows what part two will ask :)
+export function isInvalid(input: number) {
+  const val = input.toString();
+  const invalidSubstringSize = 2;
+
+  for (let i = 0; i < val.length; i++) {
+    const sub = val.substring(0, i)
+    const regex = new RegExp(`^(${sub}){${invalidSubstringSize}}$`)
+    const matches = regex.exec(val);
+
+    if (Array.isArray(matches) && matches.length > 0) {
+      if (matches[0].length !== matches[1].length) {
+        return true;
+      }
+    }
+  }
+  return false
+}
+
+/**
+ * Good news, the regex method applies itself for part two :)
+ * It is, however, super slow - as expected. :(
+ */
+export function isInvalidPartTwo(input: number) {
+  const val = input.toString();
+
+  for (let i = 0; i < val.length; i++) {
+    const sub = val.substring(0, i)
+    const regex = new RegExp(`^(${sub})+$`)
+    const matches = regex.exec(val);
+
+    if (Array.isArray(matches) && matches.length > 0) {
+      if (matches[0].length !== matches[1].length) {
+        return true;
+      }
+    }
+  }
+  return false
+}
+
+export default async function solver(inputFile: string | FsPathLike): Promise<Day2Solution> {
+  const input = (await readInputFile(inputFile)).split(',');
+  const ranges = input.map((str) => str.split('-'))
+
+  const valid_1: number[] = []
+  const invalid_1: number[] = []
+  const valid_2: number[] = []
+  const invalid_2: number[] = []
+
+  ranges.forEach((range) => {
+    const start = parseInt(range[0], 10);
+    const end = parseInt(range[1], 10);
+
+    for (let i = start; i <= end; i++) {
+      if (isInvalid(i)) {
+        invalid_1.push(i)
+      } else {
+        valid_1.push(i)
+      }
+
+      if (isInvalidPartTwo(i)) {
+        invalid_2.push(i)
+      } else {
+        valid_2.push(i)
+      }
+    }
+  });
+
+  // console.log('VALID: ', valid_1, '\n\n', 'INVALID: ', invalid_1)
+  // console.log('VALID: ', valid_2, '\n\n', 'INVALID: ', invalid_2)
+
+  return {
+    partOne: invalid_1.reduce((prev, cur, i) => prev + cur, 0),
+    partTwo: invalid_2.reduce((prev, cur, i) => prev + cur, 0),
+  }
+}
