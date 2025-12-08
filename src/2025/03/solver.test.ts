602b6ba75e1ea34dc12eec53953c86e3dc62a973
Mike A. Louro <mike.a.louro@gmail.com>
2025-12-08 00:20:47 -0500

    [2025][Day 3] Part two solution
    Recursion

--- src/2025/03/solver.test.ts
+++ src/2025/03/solver.test.ts
@@ -1,23 +1,23 @@
-import solver from './solver';
+import { partOneSolver, partTwoSolver } from './solver';
 
 describe('Day 3', () => {
   describe('Part One', () => {
     test('solves correctly with sample input', async () => {
-      expect((await solver('src/2025/03/fixtures/sample.txt')).partOne).toBe(357);
+      expect(await partOneSolver('src/2025/03/fixtures/sample.txt')).toBe(357);
     });
 
     test('solves correctly with puzzle input', async () => {
-      expect((await solver('src/2025/03/star.txt')).partOne).toBe(17107);
+      expect(await partOneSolver('src/2025/03/star.txt')).toBe(17107);
     });
   });
 
-  describe.skip('Part Two', () => {
+  describe.only('Part Two', () => {
     test('solves correctly with sample input', async () => {
-      expect((await solver('src/2025/03/sample1.txt')).partTwo).toBe(0);
+      expect(await partTwoSolver('src/2025/03/fixtures/sample.txt')).toBe(3121910778619);
     });
 
     test('solves correctly with puzzle input', async () => {
-      expect((await solver('src/2025/03/star.txt')).partTwo).toBe(0);
+      expect(await partTwoSolver('src/2025/03/star.txt')).toBe(169349762274117);
     });
   });
 });
--- src/2025/03/solver.ts
+++ src/2025/03/solver.ts
@@ -1,4 +1,4 @@
-/* cspell:words joltage */
+/* cspell:words joltage, joltages */
 import { readInputFile, FsPathLike } from '@helpers/fs';
 
 type Day3Solution = {
@@ -6,7 +6,7 @@ type Day3Solution = {
   partTwo: any;
 };
 
-export default async function solver(inputFile: string | FsPathLike): Promise<Day3Solution> {
+export async function partOneSolver(inputFile: string | FsPathLike): Promise<Day3Solution['partOne']> {
   const batteryBanks: string[] = (await readInputFile(inputFile)).split('\n');
   const largestJoltagePairs: number[] = [];
 
@@ -34,8 +34,46 @@ export default async function solver(inputFile: string | FsPathLike): Promise<Da
     largestJoltagePairs.push(largestPair);
   });
 
-  return {
-    partOne: largestJoltagePairs.reduce((prev, cur) => prev + cur, 0),
-    partTwo: null,
+  return largestJoltagePairs.reduce((prev, cur) => prev + cur, 0);
+}
+
+function recurse(maxNumberNeeded: number, numberNeeded: number, batteryBank: number[], output: number[]) {
+  if (output.length === maxNumberNeeded) {
+    return output;
   }
+  const bankToCheck = [...batteryBank.slice(0, batteryBank.length - numberNeeded + 1)];
+
+  // console.info('Bank to check: ', bankToCheck);
+
+  let largest = 0;
+  let largestIndex = 0;
+
+  bankToCheck.forEach((val, index) => {
+    if (val > largest) {
+      largest = val;
+      largestIndex = index;
+    }
+  });
+
+  output.push(largest);
+
+  const nextBankToCheck = [...batteryBank.slice(largestIndex + 1)];
+
+  return recurse(maxNumberNeeded, maxNumberNeeded - output.length, nextBankToCheck, output);
+}
+
+export async function partTwoSolver(inputFile: string | FsPathLike): Promise<Day3Solution['partTwo']> {
+  let numberOfBatteriesNeeded = 12;
+  const batteryBanks: string[] = (await readInputFile(inputFile)).split('\n');
+  const joltages: number[] = [];
+
+  batteryBanks.forEach((bank) => {
+    const joltage = recurse(
+      numberOfBatteriesNeeded, numberOfBatteriesNeeded, [...bank].map((v) => parseInt(v, 10)), []
+    ).reduce((s, n) => s.toString() + n.toString(), '')
+
+    joltages.push(parseInt(joltage, 10));
+  });
+
+  return joltages.reduce((cur, next) => cur + next, 0);
 }
