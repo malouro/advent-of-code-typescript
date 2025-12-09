4d4cd8e98cddccdebf26453e247d74ba32b92650
Mike A. Louro <mike.a.louro@gmail.com>
2025-12-08 23:44:53 -0500

    [2025][Day 4] Add part two solution + tests

--- src/2025/04/solver.test.ts
+++ src/2025/04/solver.test.ts
@@ -3,21 +3,21 @@ import solver from './solver';
 describe('Day 4', () => {
   describe('Part One', () => {
     test('solves correctly with sample input', async () => {
-      expect((await solver('src/2025/04/sample1.txt')).partOne).toBe(0);
+      expect((await solver('src/2025/04/fixtures/sample.txt')).partOne).toBe(13);
     });
 
     test('solves correctly with puzzle input', async () => {
-      expect((await solver('src/2025/04/star.txt')).partOne).toBe(0);
+      expect((await solver('src/2025/04/star.txt')).partOne).toBe(1540);
     });
   });
 
   describe('Part Two', () => {
     test('solves correctly with sample input', async () => {
-      expect((await solver('src/2025/04/sample1.txt')).partTwo).toBe(0);
+      expect((await solver('src/2025/04/fixtures/sample.txt')).partTwo).toBe(43);
     });
 
     test('solves correctly with puzzle input', async () => {
-      expect((await solver('src/2025/04/star.txt')).partTwo).toBe(0);
+      expect((await solver('src/2025/04/star.txt')).partTwo).toBe(8972);
     });
   });
 });
--- src/2025/04/solver.ts
+++ src/2025/04/solver.ts
@@ -1,17 +1,95 @@
 import { readInputFile, FsPathLike } from '@helpers/fs';
 
 type Day4Solution = {
-  partOne: unknown;
+  partOne: number;
   partTwo: unknown;
 };
 
-export default async function solver(inputFile: string | FsPathLike): Promise<Day4Solution> {
-  const input = (await readInputFile(inputFile)).split('\n');
+const ROLL_CHAR = '@';
+const EMPTY_CHAR = '.';
 
-  console.log(input);
+function checkAdjacentRolls(grid: string[][], posX: number, posY: number): number {
+  let rollCount = 0;
+  const onLeftEdge = posX === 0;
+  const onRightEdge = posX === grid[0].length - 1;
+  const onTopEdge = posY === 0;
+  const onBottomEdge = posY === grid.length - 1;
+
+  const accRollCount = (offsetX: number, offsetY: number): void => {
+    const spotToCheck = grid[posY + offsetY][posX + offsetX];
+    const isARoll = spotToCheck === ROLL_CHAR;
+
+    rollCount += isARoll ? 1 : 0;
+  };
+
+  for (let x = -1; x <= 1; x++) {
+    for (let y = -1; y <= 1; y++) {
+      if (x === 0 && y === 0) {
+        // rollCount++;
+      } else if (
+        !(x === -1 && onLeftEdge) &&
+        !(x === 1 && onRightEdge) &&
+        !(y === -1 && onTopEdge) &&
+        !(y === 1 && onBottomEdge)
+      ) {
+        accRollCount(x, y);
+      }
+    }
+  }
+
+  return rollCount;
+}
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+function recurse(grid: string[][], count: number) {
+  const liftableRolls: Position[] = [];
+
+  grid.forEach((row, y) => {
+    row.forEach((_col, x) => {
+      if (grid[y][x] === ROLL_CHAR) {
+        if (checkAdjacentRolls(grid, x, y) < 4) {
+          count++;
+          liftableRolls.push({ x, y });
+        }
+      }
+    });
+  });
+
+  if (liftableRolls.length === 0) {
+    return count;
+  }
+
+  // remove rolls
+  for (const position of liftableRolls) {
+    grid[position.y][position.x] = EMPTY_CHAR;
+  }
+
+  return recurse(grid, count);
+}
+
+export default async function solver(inputFile: string | FsPathLike): Promise<Day4Solution> {
+  const grid: string[][] = (await readInputFile(inputFile)).split('\n').map((row) => [...row]);
+
+  let partOneCount = 0;
+
+  grid.forEach((row, y) => {
+    row.forEach((_col, x) => {
+      if (grid[y][x] === ROLL_CHAR) {
+        if (checkAdjacentRolls(grid, x, y) < 4) {
+          partOneCount++;
+        }
+      }
+    });
+  });
+
+  const partTwoCount = recurse(grid, 0);
 
   return {
-    partOne: null,
-    partTwo: null,
+    partOne: partOneCount,
+    partTwo: partTwoCount,
   };
 }
